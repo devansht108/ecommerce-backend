b370414ab1a73660353e36a1f8fb1e34d50a5e56
devansht108 <tiwaridevansh21@gmail.com>
2025-11-30 23:13:38 +0530

    feat: added mock payment and order finalization (Day 17)

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware.js";
+import { markOrderPaid } from "../controllers/orderController.js";
 import { createOrder } from "../controllers/orderController.js";
 
 const router = express.Router();
 
 router.post("/", protect, createOrder);
+router.patch("/:id/pay", protect, markOrderPaid);
 
 export default router;
